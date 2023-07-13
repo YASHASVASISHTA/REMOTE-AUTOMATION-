c96b367496af0067116b86fa4c03ff1088fb0edc
YASHASVASISHTA <112754882+YASHASVASISHTA@users.noreply.github.com>
2023-07-13 16:39:07 +0530

    Add files via upload

---
+++ remote automation.py
@@ -0,0 +1,96 @@
+import imapclient,pyzmail,webbrowser,subprocess,pyautogui,threading,time,os,datetime
+import yagmail as email
+import pywhatkit as kit
+
+def get_email():
+    
+    dict={'Chrome':"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"}
+    server=imapclient.IMAPClient('imap.gmail.com',ssl=True)
+    server.login('mahima18vasishta@gmail.com','zdjanuyjwwgtqybp')
+    server.select_folder('INBOX',readonly=False)
+    UIDs=server.search(['FROM' ,'Yashas Vasishta <yashas752004@gmail.com>'])
+    print(UIDs)
+    instructions=[]
+    for uid in UIDs:
+        rawMessage=server.fetch(uid,['BODY[]','FLAGS'])
+        message=pyzmail.PyzMessage.factory(rawMessage[uid] [b'BODY[]'])
+    
+        if message.text_part != None:
+            print(message.text_part.get_payload().decode(message.text_part.charset))
+            instructions.append(message.text_part.get_payload().decode(message.text_part.charset))
+            
+    server.delete_messages(uid)
+    server.logout()
+   
+
+    for instruction in instructions:
+        print(instruction.split())
+        for instruct in instruction.split():
+            print(instruct)
+            if 'torrent' in str(instruct) or str(instruct).startswith("magnet:?"):
+                torrent=subprocess.Popen(["C:\Program Files\qBittorrent\qbittorrent.exe",str(instruct)])
+
+                if torrent.poll() == None:
+                    print('Process is still running')
+                    time.sleep(10)
+                    pyautogui.press('enter')
+                    send_confirmation('qBittorrent',f"qBittorrent is up and running.")
+
+            
+                
+            
+            for app in dict.keys():
+                
+                if "Openall:" in instruct :
+                    application=subprocess.Popen(dict[app])
+                    if application.poll()== None:
+                        print("Process running succesfully")
+                        send_confirmation(app,f"{app} is up and running: {app} is online!!!")
+                        
+                if  "chrome" in instruct.lower() :
+                    application=subprocess.Popen(dict[app])
+                    if application.poll()== None:
+                        print("Process running succesfully")
+                        send_confirmation(app,f"{app} is up and running: {app} is online!!!!")
+              
+                
+        
+    #screenshot=pyautogui.screenshot()
+    #screenshot.save(f"C:\\Users\\Sujay S C\\Downloads\\screen.png")
+    #send_confirmation('PC','The PC is awake and the scheduled tasks are running',"C:\\Users\\Sujay S C\\Downloads\\screen.png")
+             
+
+
+
+        
+    
+def send_confirmation(process,body,attachment=None):
+    login=email.SMTP('yashas752004@gmail.com','teoyppkimhfvzjpp')
+    if attachment:
+        login.send(to='pes1202203831@pesu.pes.edu',subject=f"{process} is running.",contents=body)
+    else:
+        login.send(to='pes1202203831@pesu.pes.edu',subject=f"{process} is running.",contents=body,attachments=attachment)
+    login.close()
+           
+
+def sos():
+    import requests
+    import json
+
+    send_url = "http://api.ipstack.com/check?access_key=9218a6e821aca6b2d9e0b6111b782759"
+    geo_req = requests.get(send_url)
+    geo_json = json.loads(geo_req.text)
+    print(geo_json)
+    latitude = geo_json['latitude']
+    longitude = geo_json['longitude']
+    city = geo_json['city']
+    print(latitude)
+    print(longitude)
+    webbrowser.open(f'https://maps.google.com/?q={latitude},{longitude}')
+    time=datetime.datetime.now()
+    kit.sendwhatmsg_instantly(phone_no='+917569672112',message=f'This is an SOS from yashas ,this is his latest location https://maps.google.com/?q={latitude},{longitude} ')
+
+            
+#get_email()
+#sos()
+kit.sendwhats_image("+917569672112", "a.png")         
